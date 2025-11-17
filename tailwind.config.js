5c62b2b712c28396322971556f998974704a4d90
unknownmillionaire2004 <unknownmillionaire2004@gmail.com>
2025-11-18 03:28:11 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ['./pages/**/*.{js,jsx}','./components/**/*.{js,jsx}'],
-  theme: { extend: { colors: { 'zion-teal':'#0D9488','zion-navy':'#0A3D62' } } },
-  plugins: []
-}
