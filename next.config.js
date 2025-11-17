75753054e8afa7f28085fda0ad7259826368314b
unknownmillionaire2004 <unknownmillionaire2004@gmail.com>
2025-11-18 03:27:33 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['placehold.co','images.unsplash.com','via.placeholder.com','unpkg.com']
-  }
-}
-module.exports = nextConfig
